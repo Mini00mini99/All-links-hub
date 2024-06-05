30995f8a425087049acb403c7e59a680b783f30b
Mini00mini99 <mini00mini99@gmail.com>
2024-06-05 23:34:59 +0600

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://linkshub.free.nf
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
