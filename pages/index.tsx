e8db7d3e6776a67e8efcadbf46acb45143793e92
Mini00mini99 <mini00mini99@gmail.com>
2024-06-06 09:47:13 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://linkshub.free.nf/">Next.js!</a>
+          Just <a href="https://linkshub.free.nf/">click here</a>
         </h1>
 
         <p className={styles.description}>
